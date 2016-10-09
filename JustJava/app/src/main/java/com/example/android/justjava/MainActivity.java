99465a57410da54377da1d778655feb0a776b525
renansmoura <renansmoura@hotmail.com>
2016-10-09 20:02:25 -0300

    End leason 2b -  Finish Project

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void submitOrder(View view){
-       
         displayPrice(quantity*3);
 
     }
@@ -40,8 +39,12 @@ public class MainActivity extends AppCompatActivity {
     }
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        priceTextView.setText("Total : "+NumberFormat.getCurrencyInstance().format(number) + "\nThank You!");
 
+    }
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
 
